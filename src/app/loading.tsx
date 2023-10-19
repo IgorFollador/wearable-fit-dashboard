6cd41d4a039b1a69a894389c2064849eaf33582a
IgorFollador <igorledf@hotmail.com>
2023-10-19 00:24:17 -0300

    Add pulse in Loading image

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,18 +1,21 @@
 import Image from "next/image";
 import logo from "../../public/assets/logo.svg";
 
-export default function Loading() {
+const Loading = () => {
   return (
     <div className="loading-container">
       <div className="loading-content">
-        <Image
-          src={logo}
-          alt="Wearable FIT Logo"
-          width={400}
-          className="m-auto"
-          priority
-        />
+        <div className="logo-container">
+          <Image
+            src={logo}
+            alt="Wearable FIT Logo"
+            width={400}
+            className="m-auto animate-pulse"
+          />
+        </div>
       </div>
     </div>
   );
-}
+};
+
+export default Loading;
