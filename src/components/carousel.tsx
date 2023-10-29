b5594ab21c1d70874b4586af1ce498a11dddce0b
IgorFollador <igorledf@hotmail.com>
2023-10-29 19:05:36 -0300

    Create autoplay home carousel

--- src/components/carousel.tsx
+++ src/components/carousel.tsx
@@ -1,21 +1,26 @@
 import React, { useEffect, useState } from 'react';
-import { RxDotFilled } from 'react-icons/rx'
-import image1 from '../../public/assets/home/woman_run.png'
-import image2 from '../../public/assets/home/wearable.png'
-import image3 from '../../public/assets/home/bodybuilding.png'
+import { RxDotFilled } from 'react-icons/rx';
+import image1 from '../../public/assets/home/woman_run.png';
+import image2 from '../../public/assets/home/wearable.png';
+import image3 from '../../public/assets/home/bodybuilding.png';
 
 export function Carousel() {
-  const slides = [
-    image1,
-    image2,
-    image3
-  ];
-
+  const slides = [image1, image2, image3];
   const [currentIndex, setCurrentIndex] = useState(0);
 
   const goToSlide = (slideIndex) => {
     setCurrentIndex(slideIndex);
   };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % slides.length);
+    }, 5000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
 
   return (
     <div className='max-w-[1400px] h-[600px] w-full m-auto py-16 px-4 relative group'>
@@ -28,7 +33,9 @@ export function Carousel() {
           <div
             key={slideIndex}
             onClick={() => goToSlide(slideIndex)}
-            className='text-2xl cursor-pointer'
+            className={`text-2xl cursor-pointer ${
+              currentIndex === slideIndex ? 'text-blue-500' : 'text-gray-500'
+            }`}
           >
             <RxDotFilled />
           </div>
