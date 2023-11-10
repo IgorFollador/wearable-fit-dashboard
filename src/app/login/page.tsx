4611e93d916ed2ce9f17e43940883c52bea6e325
IgorFollador <igorledf@hotmail.com>
2023-11-10 20:33:42 -0300

    Fix layout in login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,20 +1,28 @@
-'use client'
-
 import { Metadata } from "next";
 
 import Link from "next/link";
 import UserLoginForm from "@/components/user-login-auth";
+import Image from "next/image";
+import logo from "@/../public/assets/logo.svg";
 
-// export const metadata: Metadata = {
-//   title: "Autenticação",
-//   description:
-//     "Página de autenticação na plataforma Wearable FIT",
-// };
+export const metadata: Metadata = {
+  title: "Autenticação - Wearable FIT",
+  description:
+    "Página de autenticação na plataforma Wearable FIT",
+};
 
 export default function Login() {
   return (
-    <div className="container py-48 md:py-0  h-full flex-col items-center justify-center md:grid">
-      <div className="lg:p-8">
+    <div className="container lg:py-48 md:py-0  h-full flex-col items-center justify-center md:grid">
+      <div className=" h-full">
+        <div className="w-full flex justify-center">
+          <Image
+            src={logo}
+            alt="Wearable FIT Icon"
+            width={300}
+            priority
+          />
+        </div>
         <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
           <div className="flex flex-col space-y-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">Entrar</h1>
